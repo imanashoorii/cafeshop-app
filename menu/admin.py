918f0f08e3e58486d409f7b63812758b97e652da
Iman Ashoorii <imanashoorii.77@gmail.com>
2022-01-25 16:11:02 +0330

    models choices

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Menu, Category
+from .models import Menu, Category, Type
 
 admin.site.register(Menu)
 admin.site.register(Category)
+admin.site.register(Type)
--- menu/models.py
+++ menu/models.py
@@ -3,13 +3,13 @@ from django.db import models
 
 class Category(models.Model):
     CHOICES = [
-        ('Appetizer', 'پیش غذا'),
-        ('Tea and coffee', 'چای و قهوه'),
-        ('Deserts', 'دسرها'),
-        ('Main Food', 'غذاهای اصلی'),
-        ('Cold Drinks', 'نوشیدنی های سرد'),
+        (1, 'پیش غذا'),
+        (2, 'چای و قهوه'),
+        (3, 'دسرها'),
+        (4, 'غذاهای اصلی'),
+        (5, 'نوشیدنی های سرد'),
     ]
-    name = models.CharField(max_length=100, choices=CHOICES)
+    name = models.IntegerField(choices=CHOICES)
 
     class Meta:
         ordering = ('name',)
@@ -17,18 +17,28 @@ class Category(models.Model):
         verbose_name_plural = "categories"
 
     def __str__(self):
-        return self.name
+        return self.get_name_display()
 
 
 class Type(models.Model):
-    CHOICES = [
-        ('Appetizer', 'پیش غذا'),
-        ('Tea and coffee', 'چای و قهوه'),
-        ('Deserts', 'دسرها'),
-        ('Main Food', 'غذاهای اصلی'),
-
-    ]
-    name = models.CharField(max_length=100, choices=CHOICES)
+    CHOICES = (
+        (1, 'ماکتیل ها'),
+        (2, 'نوشیدنی های سالم'),
+        (3, 'شیک ها'),
+        (4, 'غذاهای اصلی'),
+        (5, 'پیشنهاد باریستا'),
+        (6, 'دمنوش های الهام بخش'),
+        (7, 'دسرها'),
+        (8, 'وافل/کرپ'),
+        (9, 'پیتزا و پاستا'),
+        (10, 'ساندویج و برگر'),
+        (11, 'استیک'),
+        (12, 'غذای ایرانی'),
+        (13, 'صبحانه'),
+        (14, 'سالاد'),
+        (15, 'سوپ'),
+    )
+    name = models.IntegerField(choices=CHOICES)
 
     class Meta:
         ordering = ('name',)
