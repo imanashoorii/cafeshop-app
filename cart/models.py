3f12098aa15760aad41b8784938ff4376bea6ad1
Iman Ashoorii <imanashoorii.77@gmail.com>
2022-01-26 12:00:31 +0330

    add cart models

--- cart/models.py
+++ cart/models.py
@@ -1,2 +1,32 @@
 from django.db import models
+from users.models import User
+from menu.models import Menu
 
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.name}"
+
+    def getFinalPrice(self):
+        return self.quantity * self.item.price
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    orderedAt = models.DateTimeField()
+
+    def __str__(self):
+        return self.user.username
+
+    def get_total_price(self):
+        total = 0
+        for order_item in self.items.all():
+            total += order_item.getFinalPrice()
+        return total
--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from core import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("account/", include("users.urls")),
-    path("", include("menu.urls"))
+    path("cart/", include("cart.urls")),
+    path("", include("menu.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
